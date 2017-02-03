bbadd25d401a5e58e9d09983538eb084744d7ee5
eugene <novikov.eugeniy@gmail.com>
2017-02-03 20:07:55 +0400

    проработка webpack

---
+++ ui/index.js
@@ -0,0 +1,4 @@
+var moment = require('moment');
+var uuid = require('uuid');
+console.log(moment().format());
+console.log(uuid.v4());
--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,16 +1,34 @@
-'use strict';
 var webpack = require('webpack');
 var path = require('path');
+var fs = require('fs');
 
-module.exports = function(env) {
-    return {
+// упаковка сторонних библиотек в единный файл
+var contents = fs.readFileSync('./package.json', 'utf8');
+var pack = JSON.parse(contents);
+var libs = [];
+for (var prop in pack.dependencies) 
+{
+    libs.push(prop);
+}
+
+var extract_css = new ExtractTextPlugin('style/[name].css');
+
+
+module.exports = function() {	
+	
+	return {
         entry: {
-            main: './index.js',
-            vendor: 'moment'
+            app: './index.js',
+			vendor: libs
         },
         output: {
-            filename: '[chunkhash].[name].js',
+            filename: '[hash:5].[name].js',
             path: path.resolve(__dirname, 'dist')
-        }
-    }
+        },
+        plugins: [
+            new webpack.optimize.CommonsChunkPlugin({
+                names: ['vendor', 'manifest'] // manifest - указывает на необходимость сборки всех перечисленных библиотек в единный файл				
+            })
+        ]
+    };
 }
