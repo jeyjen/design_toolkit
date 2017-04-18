4b245dde256eef8654d498eb63ce460cf3af633c
eugene <novikov.eugeniy@gmail.com>
2017-04-18 18:52:14 +0400

    в качестве базового элемента установлен svg

--- component/form/dia.js
+++ component/form/dia.js
@@ -6,10 +6,10 @@ class dia extends component {
     constructor(props) {
         super(props);
         this.state =
-        {
-            x_offset: this.props.x_offset === undefined? 25: this.props.x_offset,
-            y_offset: this.props.y_offset === undefined? 0: this.props.y_offset
-        };
+            {
+                x_offset: this.props.x_offset === undefined? 30: this.props.x_offset,
+                y_offset: this.props.y_offset === undefined? 30: this.props.y_offset
+            };
 
     }
     componentDidMount()
@@ -43,109 +43,57 @@ class dia extends component {
             }
 
 
-            let exp = <svg width={24} height={24}
-                           style={{display:'inline-block', verticalAlign: 'middle'}}>
-                <use
-                    key={key++}
-                    xlinkHref={'#_empty'}
-                    transform="scale(1)"
-                    style={{display:'inline-block'}}
-                     />
-            </svg>;
+            els.push(<use key={key++} xlinkHref={'#' + v.type} transform="scale(1)" x={this.x(v.x)} y={this.y(v.y)} style={{stroke:c, fill:c}} onDoubleClick={this.node_double_click(v.id)} onClick={this.node_click(v.id)} />);
+            lbls.push(<text key={key++} x={this.x(v.x) + 25} y={this.y(v.y) + 12} dy="5" style={{fill:c}} onClick={this.node_click(v.id)}>{v.name}</text>);
+
             if(v.expand_state == vs.c.expand_state.EXPANDED)
             {
-                exp =
-                    <svg width={24} height={24}
-                         style={{display:'inline-block', verticalAlign: 'middle'}}
-                    >
-                        <use
-                            key={key++}
-                            xlinkHref={'#_expanded'}
-                            transform="scale(1)"
-                            style={{stroke:color, fill:color, display:'inline-block'}}
-                            onClick={this.node_collapse.bind(v.id)} />
-                    </svg>;
+                expands.push(<use key={key++} xlinkHref={'#_expanded'} transform="scale(1)" x={this.x(v.x)-20} y={this.y(v.y)} style={{stroke:color, fill:color}} onClick={this.node_collapse.bind(v.id)} />);
             }
             else if(v.expand_state == vs.c.expand_state.COLLAPSED)
             {
-                exp =
-                    <svg width={24} height={24}
-                         style={{display:'inline-block', verticalAlign: 'middle'}}
-                    >
-                        <use
-                            key={key++}
-                            xlinkHref={'#_collapsed'}
-                            transform="scale(1)"
-                            style={{ stroke:color, fill:color, display:'inline-block'}}
-                            onClick={this.node_expand.bind(v.id)}/>
-                    </svg>;
+                expands.push(<use key={key++} xlinkHref={'#_collapsed'} transform="scale(1)" x={this.x(v.x)-20} y={this.y(v.y)} style={{stroke:color, fill:color}} onClick={this.node_expand.bind(v.id)}/>);
             }
-
-            els.push(
-                <div style={{paddingLeft:this.x(v.x) + 50, paddingTop:3, paddingBottom:3, position:'relative'}}>
-                    <a style={{display:'inline-block', position:'absolute', left:this.x(v.x)}}>
-                        {exp}
-                        <svg width={24} height={24}
-                             style={{ display:'inline-block', verticalAlign: 'middle'}}
-                             onDoubleClick={this.node_double_click(v.id)}>
-                            <use key={key++}
-                                 xlinkHref={'#' + v.type}
-                                 style={{stroke:c, fill:c}}
-                                 onClick={this.node_click(v.id)}/>
-                        </svg>
-                    </a>
-                    <div
-                        contentEditable={true}
-                        style={{position:'relative', display:'block', marginLeft:5, outline: 'none', wordWrap: 'break-word'}}>
-                                {v.name}
-                    </div>
-
-                </div>);
-
-            //els.push(<use key={key++} xlinkHref={'#' + v.type} transform="scale(1)"  x={this.x(v.x)} y={this.y(v.y)} style={{stroke:c, fill:c}} onDoubleClick={this.node_double_click(v.id)} onClick={this.node_click(v.id)} />);
-            //lbls.push(<text key={key++} x={this.x(v.x) + 15} y={this.y(v.y)} dy="5" style={{fill:c}} onClick={this.node_click(v.id)}>{v.name}</text>);
-
-            // if(v.expand_state == vs.c.expand_state.EXPANDED)
-            // {
-            //     expands.push(<use key={key++} xlinkHref={'#_expanded'} transform="scale(1)" x={this.x(v.x)-20} y={this.y(v.y)} style={{stroke:color, fill:color}} onClick={this.node_collapse.bind(v.id)} />);
-            // }
-            // else if(v.expand_state == vs.c.expand_state.COLLAPSED)
-            // {
-            //     expands.push(<use key={key++} xlinkHref={'#_collapsed'} transform="scale(1)" x={this.x(v.x)-20} y={this.y(v.y)} style={{stroke:color, fill:color}} onClick={this.node_expand.bind(v.id)}/>);
-            // }
-
         });
 
-        // let links = vs.refs.map((i)=>
-        // {
-        //     let d = "";
-        //
-        //     if(i.x2 > i.x1)// для потомков
-        //     {
-        //         d += "M" + (this.x(i.x1) + 8) + " " + (this.y(i.y1) + 8);
-        //         d += " L " + (this.x(i.x2) - 9) + " " + (this.y(i.y2) - 9);
-        //     }
-        //     else if(i.x2 == i.x1) // для узлов на одном уровне
-        //     {
-        //         d += "M" + this.x(i.x1) + " " + (this.y(i.y1) + 11);
-        //         d += " L " + this.x(i.x2) + " " + (this.y(i.y2) - 12);
-        //     }
-        //     else if(i.x2 < i.x1 && i.y2 > i.y1 )// для следующих на уровень выше
-        //     {
-        //         d += "M" + (this.x(i.x1) - 8) + " " + (this.y(i.y1) + 8);
-        //         d += " L " + (this.x(i.x2) + 9) + " " + (this.y(i.y2) - 9);
-        //     }
-        //     else if(i.x2 < i.x1 && i.y2 < i.y1) // для циклов (возврат к предыдущим на уровень выше)
-        //     {
-        //
-        //     }
-        //     return <path key={key++} d={d} stroke={'#CFD8DC'} strokeWidth={1} fill="none" markerEnd="url(#arrow)"></path>
-        // })
+        let links = vs.refs.map((i)=>
+        {
+            let d = "";
+
+            if(i.x2 > i.x1)// для потомков
+            {
+                d += "M" + (this.x(i.x1) + 8) + " " + (this.y(i.y1) + 8);
+                d += " L " + (this.x(i.x2) - 9) + " " + (this.y(i.y2) - 9);
+            }
+            else if(i.x2 == i.x1) // для узлов на одном уровне
+            {
+                d += "M" + this.x(i.x1) + " " + (this.y(i.y1) + 11);
+                d += " L " + this.x(i.x2) + " " + (this.y(i.y2) - 12);
+            }
+            else if(i.x2 < i.x1 && i.y2 > i.y1 )// для следующих на уровень выше
+            {
+                d += "M" + (this.x(i.x1) - 8) + " " + (this.y(i.y1) + 8);
+                d += " L " + (this.x(i.x2) + 9) + " " + (this.y(i.y2) - 9);
+            }
+            else if(i.x2 < i.x1 && i.y2 < i.y1) // для циклов (возврат к предыдущим на уровень выше)
+            {
+
+            }
+            return <path key={key++} d={d} stroke={'#CFD8DC'} strokeWidth={1} fill="none" markerEnd="url(#arrow)"></path>
+        })
 
         return (
-                <div className={this.props.className} style={{width:'100%', height:'99%'}} onClick={()=>{ vs.select_node(null);}}>
-                    {els}
-                </div>
+            <svg className={this.props.className} style={{width:'100%', height:400}} onClick={()=>{ vs.select_node(null);}}>
+                <defs>
+                    <marker id="arrow" viewBox=" 0 0 10 10" markerWidth="5" markerHeight="5" refX="3" refY="3" orient="auto" markerUnits="strokeWidth">
+                        <path d="M0,0 L0,6 L6,3 z" fill={color} />
+                    </marker>
+                </defs>
+                {links}
+                {els}
+                {lbls}
+                {expands}
+            </svg>
         );
     }
 
@@ -160,7 +108,6 @@ class dia extends component {
 
     node_double_click(node_id)
     {
-
         return function (e, id) {
             e.persist();
             e.stopPropagation();
@@ -169,7 +116,6 @@ class dia extends component {
             //console.log(e);
             //console.log(this);
             //vs.select_node(node_id);
-            alert("ok");
             vs.scale_to_node(node_id);
         }.bind(this)
     }
@@ -198,21 +144,3 @@ class dia extends component {
     }
 }
 export default dia;
-
-/*
-
-* */
-
-
-/*
- <use xlinkHref="#if" y="50" x="50" style={{stroke:'#ffffff', fill:'#ffffff'}} />
- <use xlinkHref="#else" y="80" x="50" style={{stroke:'#ffffff', fill:'#ffffff'}}/>
- <use xlinkHref="#switch" y="110" x="50" style={{stroke:'#ffffff', fill:'#ffffff'}}/>
- <use xlinkHref="#switch" y="140" x="50" style={{stroke:'#ffffff', fill:'#ffffff'}}/>
- <use xlinkHref="#switch" y="170" x="50" style={{stroke:'#ffffff', fill:'#ffffff'}}/>
- <use xlinkHref="#op" y="200" x="50" style={{stroke:'#ffffff', fill:'#ffffff'}}/>
- <use xlinkHref="#and" y="230" x="50" style={{stroke:'#ffffff', fill:'#ffffff'}}/>
- <use xlinkHref="#or" y="260" x="50" style={{stroke:'#ffffff', fill:'#ffffff'}}/>
- <use xlinkHref="#call" y="290" x="50" style={{stroke:'#ffffff', fill:'#ffffff'}}/>
- <use xlinkHref="#while" y="320" x="50" style={{stroke:'#ffffff', fill:'#ffffff'}}/>
-* */
